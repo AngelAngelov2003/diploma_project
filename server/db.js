3c4b3aabe08b466ca9f3326eb389ecf0f3256172
AngelAngelov2003 <angel1aa2a2@gmail.com>
2026-03-25 19:43:57 +0200

    Fixed issue

--- server/db.js
+++ server/db.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
+  user: 'postgres',
+  host: 'localhost',
+  database: 'fishing_db',
   password: process.env.DB_PASSWORD, 
-  port: process.env.DB_PORT,
+  port: 5432,
 });
 
 module.exports = pool;
